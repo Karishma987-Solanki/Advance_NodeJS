78746ba6def074c8b569ce5b2eac73b4da8f5640
Karishma Solanki <karishmasolanki987@gmail.com>
2023-08-15 01:20:48 +0530

    Created update-product and delete-product API

--- index.js
+++ index.js
@@ -45,6 +45,39 @@ app.get("/get-products", async (req, res) => {
   }
 });
 
+app.put("/update-product", async (req, res) => {
+  try {
+    const { id } = req.query;
+    const product = req.body;
+    await Product.updateOne(
+      { _id: id },
+      {
+        $set: {
+          product_id: product.product_id,
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          image: product.image,
+        },
+      }
+    );
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    res.status(500).json({ message: "failure" });
+  }
+});
+
+app.delete("/delete-product", async (req, res) => {
+  try {
+    const { id } = req.query;
+    await Product.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    res.status(500).json({ message: "failure" });
+  }
+});
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
