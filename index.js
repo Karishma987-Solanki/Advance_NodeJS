53b0fecac18c2d340fb7e1fbadde250764342981
Daman Bhola <damanbhola1@gmail.com>
2023-08-14 19:57:06 +0200

    Connection with MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
+
+const mongodbUrl = process.env.MONGODB_URL || "";
+
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
